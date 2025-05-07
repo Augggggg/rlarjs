14b57095889ca8547f6f25c256876cbc991754ee
김건 <25_09@bssm.hs.kr>
2025-05-07 18:02:34 +0900

    수정

--- test1.py
+++ test1.py
@@ -2,7 +2,7 @@ import streamlit as st
 col1,col2 = st.columns([2,3])
 
 with col1 :
-    st.tilte('here is column1')
+    st.title('here is column1')
 with col2 :
     st.title('here is column2')
     st.checkbox('this is checkbox1 in col2')
